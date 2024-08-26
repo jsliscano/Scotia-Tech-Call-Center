49376012eb629d9eae2d4a87e854617be482fa70
Estiven liscano <estivenliscano2017@gmail.com>
2024-08-26 07:54:07 -0500

    Creation of backend base project

---
+++ src/main/java/com/scotiatech/demo/ScotiaApplication.java
@@ -0,0 +1,13 @@
+package com.scotiatech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScotiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScotiaApplication.class, args);
+	}
+
+}
